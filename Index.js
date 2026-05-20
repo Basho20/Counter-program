4d682ee4ffd866ea19e86d025e1237505b34d4eb
Basho20 <mohamedbash19574@gmail.com>
2026-05-20 05:16:41 -0400

    Else if statements

--- Index.js
+++ Index.js
@@ -1,23 +1,38 @@
-// COUNTER PROGRAM
+// IF STATEMENTS = if a condition is true, then do something. If the condition is false, then do something else.
 
-const decreaseBtn = document.getElementById("decrease");
-const resetBtn = document.getElementById("resetBtn");
-const increaseBtn = document.getElementById("increase");
-const countLabel = document.getElementById("countlabel");
-let count = 0;
+// Example 1: Basic if statement
+// else if  statement = if the first condition is false, then check another condition. If that condition is true, then do something. If that condition is false, then do something else.
 
-increaseBtn.onclick = function() {
-    count++;
-    countLabel.textContent = count;
+
+const myText = document.getElementById("myText");
+const mySubmit = document.getElementById("mySubmit");
+const resultElement = document.getElementById("resultElement");
+let age;
+
+mySubmit.onclick = function(){
+     age = myText.value; 
+ age = Number(age);  
+    if (age > 100) {
+     resultElement.textContent = "You are too old to enter this site";
+    }
+if (age == 0) {
+ resultElement.textContent = "You cant enter. you are just born";
+}
+else if (age >= 18) {
+resultElement.textContent = "You are old enough to enter this site";
+}
+
+else if (age < 0) {
+     resultElement.textContent = "You age cannot below 0";
 }
 
 
-decreaseBtn.onclick = function() {
-    count--;
-    countLabel.textContent = count;
+else {
+     resultElement.textContent = "You must be 18+ to enter this site";
+}
 }
 
-resetBtn.onclick = function() {
-    count = 0;
-    countLabel.textContent = count;
-}
+
+
+
+
